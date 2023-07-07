f2e6e0678f659f6f30505a4892abedffc7bc5d9e
= <adrianohenriqueafonso@gmail.com>
2023-07-07 17:39:38 -0300

    Learning Kotlin Coroutines Tutorial 1-7

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -15,4 +15,5 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    groupBy { it.login }.map { (login, grouped) -> User(login, grouped.sumOf { it.contributions }) }
+        .sortedByDescending { it.contributions }
--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,17 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferred: List<Deferred<List<User>>> = repos.map { repo ->
+        async {
+            log("start loading for repo ${repo.name}")
+            delay(3000)
+            service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
+        }
+    }
+    deferred.awaitAll().flatten().aggregate()
 }
